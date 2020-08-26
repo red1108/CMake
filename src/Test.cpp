8ca9172d8b84db222586fddea32c8aae77276203
red1108 <minkyu.lee65@gmail.com>
2020-08-26 20:37:15 +0900

    제발 좀 되라

---
+++ src/Test.cpp
@@ -0,0 +1,5 @@
+#include "Test.h"
+
+int add(int a, int b){
+    return a+b;
+}
---
+++ test/AddTest.cpp
@@ -0,0 +1,8 @@
+#include "../src/Test.h"
+#include <iostream>
+using namespace std;
+
+int main(){
+    if((add(2,3)==5)) cout<<"Correct!";
+    else cout<<"Wrong!";
+}
